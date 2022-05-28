bb67e5a5b8470951aa62323a656a72fa2bc116cd
jn21 <josue.nya@gohze.org>
2022-05-28 18:45:47 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyARsvLAzpClkEL5LBPhO-Vn6-jiBO_RzWo',
+  authDomain: 'taskmanager-876aa.firebaseapp.com',
+  databaseURL: 'https://taskmanager-876aa-default-rtdb.firebaseio.com/',
+  projectId: 'taskmanager-876aa',
+  storageBucket: 'taskmanager-876aa.appspot.com',
+  messagingSenderId: '231696089797',
+  appId: '1:231696089797:web:3e10ada3be3118d4e566ac',
+  measurementId: 'G-B7CB3P2VWC'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentDate: string;
+  myTask = '';
+  addTask: boolean;
+  tasks = [];
 
-  constructor() {}
+  constructor(public afDB: AngularFireDatabase,) {
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    this.currentDate = date.toLocaleDateString('fr-FR');
+    this.getTasks();
+  }
 
+  addTaskToFirebase() {
+    this.afDB.list('Tasks/').push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    });
+    this.showForm();
+  }
+
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+
+  getTasks() {
+    this.afDB.list('Tasks/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          text: action.payload.exportVal().text,
+          hour: action.payload.exportVal().date.substring(11, 16),
+          checked: action.payload.exportVal().checked
+        });
+      });
+    });
+  }
+
+  changeCheckState(ev: any) {
+    console.log('checked: ' + ev.checked);
+    this.afDB.object('Tasks/' + ev.key + '/checked/').set(ev.checked);
+  }
+  deleteTask(task: any) {
+    this.afDB.list('Tasks/').remove(task.key);
+  }
 }
